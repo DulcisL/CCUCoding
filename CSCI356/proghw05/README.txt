4a5913c226bc13abd2cfe8db02c3b6e28caf96ba
dulcisl <dolcel307@gmail.com>
2024-12-05 12:14:12 -0500

    Added the CSCI356 proghw05 start

---
+++ CSCI356/proghw05/README.txt
@@ -0,0 +1,16 @@
+Program Homework 5
+Lakota Dolce
+CSCI 356
+Dr. Fuchs
+
+Description:
+    This project is designed to simulate a cpu scheduling algorithm
+How to run:
+    This project was built using make 3.82
+    To compile use:
+        'make'
+    Once compiled run using:
+        
+
+Known errors or bugs:
+    
---
+++ CSCI356/proghw05/app/main.c
@@ -0,0 +1,21 @@
+/*
+Lakota Dolce
+ProgHW05
+December 5, 2024
+Dr. Fuchs
+*/
+
+//Preprocessing 
+#include "my_queue.h"
+
+
+//Function Prototypes
+
+/**main
+* @param: int argc - size of the argv array
+* @param: char *argv[] - passed in values from the run command.
+* @return: int value -> 0 for success, -1 for fail
+*/
+main (int argc, char *argv[]){
+
+}
