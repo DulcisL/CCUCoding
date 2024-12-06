54f53369e2a9b5bb91c2a40a542de46bf9695709
dulcisl <dolcel307@gmail.com>
2024-12-06 10:30:03 -0500

    Added process class and header files

---
+++ CSCI356/proghw05/inc/Process.h
@@ -0,0 +1,46 @@
+/*
+ * Process - process functions for Process prototypes
+ *
+ * Author: Lakota Dolce
+ *
+ * Course: CSCI 356
+ * Version 1.0
+ */
+
+#ifndef PROCESS_H_
+#define PROCESS_H_
+
+/*Needed attributes
+    int _id;
+    int  _arrival;
+    int _cpu_time;
+    int _priority;
+
+    int _waitTime;
+    int _arrivalTime;
+    int _turnaroundTime;
+*/
+/*Needed Functions
+    void newProcess(int id, int arrivalT, int cpuT, int priority); //Paramaterized constructor
+
+    int GetID();
+    int GetArrival();
+    int GetCPUTime();
+    int GetPriority();
+
+    void SetID(id);
+    void SetArrival(arrival);
+    void SetCPUTime();
+    void SetPriority();
+    void SetWait();
+    void SetArrival();
+    void SetTurnAround();
+
+    void changePriority();
+
+    char * Tostring();
+
+
+*/
+
+#endif /* PROCESS_H_ */
---
+++ CSCI356/proghw05/src/Process.c
@@ -0,0 +1,8 @@
+/*
+ * Process - process functions for Process prototypes
+ *
+ * Author: Lakota Dolce
+ *
+ * Course: CSCI 356
+ * Version 1.0
+ */
