3e47ccbf0e31568e2c5a8540ac269f283dbd61e2
dulcisl <dolcel307@gmail.com>
2025-08-28 16:03:37 -0400

    Added CSCI490 and initial code for project

---
+++ CSCI490/Project1/src/main.js
@@ -0,0 +1,11 @@
+/* Main file for project
+    This file is to focus on the user story of a map that shows the status of the parking lots across campus
+        - "As a student I want to be able to see a visual reference of the current capacity of the parking 
+        lot to easily judge the parking across campus. An example would be a map with green, yellow, or red
+        overlayed over the parking lots on campus."
+
+*/
+function main() {
+    console.log("Hello World!")
+
+}
