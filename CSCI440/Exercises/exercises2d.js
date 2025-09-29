db1bc2abf2535cf3205b93feb05a4cd3053e0cbd
dulcisl <dolcel307@gmail.com>
2025-09-29 10:18:01 -0400

    Finished exercise2d in CSCI440

--- CSCI440/Exercises/exercises2d.js
+++ CSCI440/Exercises/exercises2d.js
@@ -1,19 +1,20 @@
 /*
-Change square colors to blue and background to grey, make 2 squares, one square stays in the same spot but gets smaller
-one square is translated directly to the right, squares are rotating in opposite directions
+Change square colors to blue and background to grey, make 2 squares, one square stays in the same spot
+one square is translated directly to the right, squares are rotating in opposite directions, both squares
+are scaled down
 */
 
 "use strict";
 
-const { mod } = require("three/tsl");
+//const { mod } = require("three/tsl");
 
 var canvas;
 var gl;
 
 var positionsArray = [];
 var colorLoc;
-var modelViewMatrix, modelViewMatrixLoc;
-var theta = 0.0;
+var modelViewMatrix, modelViewMatrix2, modelViewMatrixLoc;
+var theta = 0.00;
 
 var blue;
 
@@ -62,19 +63,43 @@ var render = function () {
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
     modelViewMatrix = mat4();
-    //translate to new location (change x values to the a negative)
-    modelViewMatrix = mult(modelViewMatrix, translate(-1.0, 0.0, 0.0));
+    //set up a copy for second square
+    modelViewMatrix2 = modelViewMatrix;
+
+    //translate to original place
+    modelViewMatrix = mult(modelViewMatrix, translate(0.4, 0.4, 0.0));
+
+    //rotation counter clockwise
+    theta -= 0.5;
+    modelViewMatrix = mult(modelViewMatrix, rotate(theta, 0, 0, 1));
+
     //scale down by factor of 2 on x and y
     modelViewMatrix = mult(modelViewMatrix, scale(0.5, 0.5, 1.0));
+
+    //translate to origin
+    modelViewMatrix = mult(modelViewMatrix, translate(-0.4, -0.4, 0.0));
+
+    //translate to new location (change x values to the a negative)
+    modelViewMatrix2 = mult(modelViewMatrix2, translate(-0.4, 0.4, 0.0));
+
+
     //rotation clockwise
-    theta += .01;
-    angle = radians(theta);
-    modelViewMatrix = mult(modelViewMatrix, rotate(angle, 2));
+    modelViewMatrix2 = mult(modelViewMatrix2, rotate(-theta, 0, 0, 1));
+
+    //scale down by factor of 2 on x and y
+    modelViewMatrix2 = mult(modelViewMatrix2, scale(0.5, 0.5, 1.0));
+
+    //translate to origin
+    modelViewMatrix2 = mult(modelViewMatrix2, translate(-0.4, -0.4, 0.0));
 
 
     // send color and matrix for square then render
     gl.uniform4fv(colorLoc, flatten(blue));
+    //render square 1
     gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
+    gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
+    //render square 2
+    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix2));
     gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
 
     requestAnimationFrame(render);
