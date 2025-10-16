eff4d5766566602708cfcc0b81d834dbbc3c585e
dulcisl <dolcel307@gmail.com>
2025-10-16 07:58:04 -0400

    Updating code for CSCI490 project, reworking to be more object oriented and to pull data from a server

---
+++ CSCI473/Code/project01/stencil.c
@@ -0,0 +1,20 @@
+/* This is the main file for the stencil project for CSCI473
+This file will create a 2d matrix, and then iterate across the matrix to simulate the passing of heat
+across a metal plate. The heat simulation will be 1 as hot and 0 as cold. The function will take the
+average of values of # of neighbors around the point of interest and assign a new value to the point
+*/
+
+// Average function
+
+// main function
+int main(int argc, char **argv)
+{
+    // Check if arguments were passed on program call
+    // Tell usage if arguments weren't passed
+    // Initialize variables and read in values from function call
+    // Create 2d matrix using # of mallocs
+    // Set boundary values
+    // Create loop to iterate user defined number of times
+    // Create loop to iterate across the values
+    // Iterate across the matrix and change values using double buffer
+}
