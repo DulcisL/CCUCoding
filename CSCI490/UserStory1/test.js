76830275efc090e043300a2d02313419c4447c9c
dulcisl <dolcel307@gmail.com>
2025-10-23 10:53:00 -0400

    Added tests to CSCI490 project

---
+++ CSCI490/UserStory1/test.js
@@ -0,0 +1,50 @@
+/*
+This file will serve as the main testing file for the project.
+*/
+
+const { exp } = require("three/tsl");
+
+//Imports
+
+//Global variables
+testCount = 0;
+failedTests = "";
+
+
+function main(){
+//initialize
+finalResult = "";
+
+//Call tests
+
+//Concatonate final result
+concat(finalResult, "Tests Complete \n Failed Tests: \n");
+concat(finalResult, failedTests);
+
+//print final result
+console.log(finalResult);
+
+}
+
+function testResults(testFunc){
+    //initialize
+    testCount += 1;
+    testName = testFunc.name;
+
+    if (testFunc != null){
+        result, expectedResult = testFunct();
+        if (result  == expectedResult){
+            console.log(testName, ": Passed")
+            return 1
+        }
+        if (result != expectedResult){
+            console.log(testName, ": Failed")
+            concat(FailedTests, testName , "\n");
+            return -1
+        }
+    }
+    if (testFunc == null) {
+        console.log("An error occurred with params");
+        return -2;
+    }
+}
